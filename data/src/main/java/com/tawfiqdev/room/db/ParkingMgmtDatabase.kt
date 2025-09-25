8b06caa9827f06b1dbb81dffae1403a39578dc2f
admin <tawfiq-externe.mouttalif@asmeg.org>
2025-09-25 16:30:29 +0200

    update class entity in ParkingMgmtDatabase

--- data/src/main/java/com/tawfiqdev/room/db/ParkingMgmtDatabase.kt
+++ data/src/main/java/com/tawfiqdev/room/db/ParkingMgmtDatabase.kt
@@ -9,16 +9,37 @@ import com.tawfiqdev.room.dao.ParkingDao
 import com.tawfiqdev.room.dao.ReservationDao
 import com.tawfiqdev.room.dao.UserDao
 import com.tawfiqdev.room.dao.VehicleDao
+import com.tawfiqdev.room.entity.EntryExitLogEntity
+import com.tawfiqdev.room.entity.FavoriteParkingEntity
 import com.tawfiqdev.room.entity.LocationEntity
 import com.tawfiqdev.room.entity.LocationFts
 import com.tawfiqdev.room.entity.ParkingEntity
+import com.tawfiqdev.room.entity.ParkingRateEntity
+import com.tawfiqdev.room.entity.ParkingSpotEntity
+import com.tawfiqdev.room.entity.PaymentEntity
 import com.tawfiqdev.room.entity.RecentLocationEntity
 import com.tawfiqdev.room.entity.ReservationEntity
+import com.tawfiqdev.room.entity.ReviewEntity
 import com.tawfiqdev.room.entity.UserEntity
 import com.tawfiqdev.room.entity.VehicleEntity
 
 @Database(
-    entities = [VehicleEntity::class , ReservationEntity::class , UserEntity::class, ParkingEntity::class, LocationEntity::class, LocationFts::class, RecentLocationEntity::class ],
+    entities = [
+        UserEntity::class,
+        ParkingEntity::class,
+        LocationEntity::class,
+        LocationFts::class,
+        RecentLocationEntity::class,
+        VehicleEntity::class,
+        ParkingEntity::class,
+        ParkingSpotEntity::class,
+        ParkingRateEntity::class,
+        ReservationEntity::class,
+        PaymentEntity::class,
+        EntryExitLogEntity::class,
+        FavoriteParkingEntity::class,
+        ReviewEntity::class
+    ],
     version = 1,
     exportSchema = false
 )
